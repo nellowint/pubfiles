7a43e0d5c5bfc9215199dc2ce203fd5c56fd442b
nellowint <wellintonvieira.office@gmail.com>
2026-06-25 16:18:21 -0300

    feat: add i18n category name translation

--- apps/categories/admin.py
+++ apps/categories/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
+from modeltranslation.admin import TabbedTranslationAdmin
 from mptt.admin import DraggableMPTTAdmin
 
 from .models import Category
 
 
 @admin.register(Category)
-class CategoryAdmin(DraggableMPTTAdmin):
+class CategoryAdmin(DraggableMPTTAdmin, TabbedTranslationAdmin):
     mptt_level_indent = 20
     list_display = ('tree_actions', 'indented_title')
     list_display_links = ('indented_title',)
---
+++ apps/categories/translation.py
@@ -0,0 +1,9 @@
+from modeltranslation.translator import TranslationOptions, register
+
+from .models import Category
+
+
+@register(Category)
+class CategoryTranslationOptions(TranslationOptions):
+    fields = ('name',)
+    empty_values = ''
