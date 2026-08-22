cc81557089dab3b087f34da2f24381eb1c70e47e
Wellinton Vieira <wellintonvieira.office@gmail.com>
2026-08-22 18:46:50 -0300

    fix: remove link_preview method to prevent navigation conflicts

--- apps/advertisements/admin.py
+++ apps/advertisements/admin.py
@@ -1,19 +1,12 @@
 from django.contrib import admin
-from django.utils.html import format_html, mark_safe
 from .models import Advertisements
 
 
 @admin.register(Advertisements)
 class AdvertisementsAdmin(admin.ModelAdmin):
-    list_display = ['link_preview', 'type', 'position', 'is_active']
+    list_display = ['__str__', 'type', 'position', 'is_active']
     list_filter = ['type', 'position', 'is_active']
     search_fields = ['link']
 
     class Media:
         js = ('js/admin-clickable-rows.js',)
-
-    def link_preview(self, obj):
-        if obj.is_script:
-            return mark_safe('<span style="color: #28a745;">Script</span>')
-        return format_html('<a href="{}" target="_blank" rel="noopener">Link</a>', obj.link)
-    link_preview.short_description = 'Conteúdo'
