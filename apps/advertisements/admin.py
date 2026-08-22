3bc664b65da42bbf90c64fc82b0e2ee0df39738c
Wellinton Vieira <wellintonvieira.office@gmail.com>
2026-08-22 17:54:29 -0300

    fix: use mark_safe for static HTML in admin preview

--- apps/advertisements/admin.py
+++ apps/advertisements/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.utils.html import format_html
+from django.utils.html import format_html, mark_safe
 from .models import Advertisements
 
 
@@ -12,6 +12,6 @@ class AdvertisementsAdmin(admin.ModelAdmin):
 
     def link_preview(self, obj):
         if obj.is_script:
-            return format_html('<span style="color: #28a745;">Script</span>')
+            return mark_safe('<span style="color: #28a745;">Script</span>')
         return format_html('<a href="{}" target="_blank" rel="noopener">Link</a>', obj.link)
     link_preview.short_description = 'Conteúdo'
