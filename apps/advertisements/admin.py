a3d843c27fecbd918bda02f6f2e86e7fb078d07a
Wellinton Vieira <wellintonvieira.office@gmail.com>
2026-08-22 18:45:24 -0300

    fix: remove unnecessary list_display_links from advertisements admin

--- apps/advertisements/admin.py
+++ apps/advertisements/admin.py
@@ -6,7 +6,6 @@ from .models import Advertisements
 @admin.register(Advertisements)
 class AdvertisementsAdmin(admin.ModelAdmin):
     list_display = ['link_preview', 'type', 'position', 'is_active']
-    list_display_links = ['link_preview']
     list_filter = ['type', 'position', 'is_active']
     search_fields = ['link']
 
